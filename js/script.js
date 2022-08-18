8ef0ac43a5a93b1b5d8d4b45795b872ea1b6b92c
Adi Kurniawan <42286406+kurniawan2805@users.noreply.github.com>
2022-08-18 17:46:19 +0700

    add function todos

--- js/script.js
+++ js/script.js
@@ -33,6 +33,64 @@ function addTodo() {
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
+function makeTodo(todoObject) {
+  const textTitle = document.createElement("h2");
+  textTitle.innerText = todoObject.task;
+
+  const textTimestamp = document.createElement("p");
+  textTimestamp.innerText = todoObject.timestamp;
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("inner");
+  textContainer.append(textTitle, textTimestamp);
+
+  const container = document.createElement("div");
+  container.classList.add("item", "shadow");
+  container.append(textContainer);
+  container.setAttribute("id", `todo-${todoObject.id}`);
+
+  if (todoObject.isCompleted) {
+    const undoButton = document.createElement("button");
+    undoButton.classList.add("undo-button");
+
+    undoButton.addEventListener("click", function () {
+      undoTaskFromCompleted(todoObject.id);
+    });
+
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-button");
+
+    trashButton.addEventListener("click", function () {
+      removeTaskFromCompled(todoObject.id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+    // not completed
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+
+    checkButton.addEventListener("click", function () {
+      addTaskToCompleted(todoObject.id);
+    });
+
+    container.append(checkButton);
+  }
+
+  return container;
+}
+
 document.addEventListener(RENDER_EVENT, function () {
-  console.log(todos);
+  // console.log(todos);
+  const uncompletedTodoList = document.getElementById("todos");
+  uncompletedTodoList.innerHTML = "";
+  // console.log(uncompletedTodoList);
+
+  for (let todoItem of todos) {
+    console.log(todoItem);
+    const todoElement = makeTodo(todoItem);
+    // console.log(todoElement);
+    uncompletedTodoList.append(todoElement);
+  }
+  // console.log(uncompletedTodoList);
 });
